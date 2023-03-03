3b091d2b4301e171b8a97ccaf502d18d68c0374e
sumit-krk <kushakk007@gmail.com>
2023-03-04 03:48:11 +0530

    some update in header

--- youtube-clone/src/components/Head.js
+++ youtube-clone/src/components/Head.js
@@ -9,23 +9,23 @@ function Head() {
     <div className='grid grid-flow-col shadow-lg p-3 bg-[#1e1d1e] font-semibold'>
         {/* header left section which contain hamburger menue and youtube icon */}
         <div className='flex col-span-1 items-center text-[24px] justify-around'>
-            <RxHamburgerMenu className='text-white' />
-            <div className='flex items-center'>
+            <RxHamburgerMenu className='text-white cursor-pointer' />
+            <div className='flex items-center cursor-pointer'>
                 <IoLogoYoutube className='text-red-500' /><span className='text-white'>Youtube</span>
             </div>
         </div>
         {/* header mid section which contain search input */}
-        <div className='flex col-span-10 items-center justify-center'>
+        <div className='flex col-span-10 items-center justify-center cursor-pointer'>
            <div className='w-3/4 flex items-center px-6'>
-                <input type='text' className='justify-center w-full p-1' style={{borderRadius:'15px 0px 0px 15px'}}/>
-                <div className='p-1 bg-gray-700 w-14 justify-center flex' style={{borderRadius:'0px 15px 15px 0px'}}>
+                <input type='text' className='justify-center w-full p-1 focus:outline-none rounded-l-full'/>
+                <div className='p-1 bg-gray-500 w-14 justify-center flex rounded-r-full'>
                     <GoSearch className='text-white text-[24px] cursor-pointer' /> 
                 </div>
            </div>
         </div>
         {/* header right section which contain user */}
         <div className='col-span-1 flex items-center'>
-            <FaUserCircle className='text-white text-[24px]' />
+            <FaUserCircle className='text-white text-[24px] cursor-pointer'  />
         </div>
     </div>
   )
