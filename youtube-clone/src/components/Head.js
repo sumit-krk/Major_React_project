824835e849199bb8d41dea1204e6576c493f8089
sumit-krk <kushakk007@gmail.com>
2023-03-04 01:46:07 +0530

    header skilton created with commented code

--- youtube-clone/src/components/Head.js
+++ youtube-clone/src/components/Head.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoLogoYoutube } from "react-icons/io";
+import { GoSearch } from "react-icons/go";
+import { FaUserCircle } from "react-icons/fa";
 
 function Head() {
   return (
-    <div>Head</div>
+    <div>
+        {/* header left section which contain hamburger menue and youtube icon */}
+        <div>
+            <RxHamburgerMenu />
+            <IoLogoYoutube className='text-red-500' />
+        </div>
+        {/* header mid section which contain search input */}
+        <div>
+           <input type='text' />
+           <GoSearch /> 
+        </div>
+        {/* header right section which contain user */}
+        <div>
+            <FaUserCircle />
+        </div>
+    </div>
   )
 }
 
