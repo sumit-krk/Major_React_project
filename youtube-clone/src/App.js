a7e994a1daf0116199881598384f10a62b6aba81
sumit-krk <kushakk007@gmail.com>
2023-03-04 01:26:53 +0530

    build the basic sklition

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Body from './components/Body';
+import Head from './components/Head';
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Sumit</h1>
+      <Head></Head>
+      <Body></Body>
     </div>
   );
 }
---
+++ youtube-clone/src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainContainer from './MainContainer'
+import Sidebar from './Sidebar'
+
+const Body = () => {
+  return (
+    <div>
+        <Sidebar></Sidebar>
+        <MainContainer></MainContainer>
+    </div>
+  )
+}
+
+export default Body
